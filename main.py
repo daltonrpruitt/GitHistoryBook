47006982b472fec3a1428fdd1ff8186bbc7afa75
Dalton P <58988139+daltonrpruitt@users.noreply.github.com>
2023-02-11 23:12:32 -0600

    Add method to output LaTeX file; build pdf, too
    Some other stuff. I like this.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from github import Github
+from github import Github, Commit
 
-from os.path import expanduser
+import os, re
 import sys, logging
+# from datetime import datetime
+from typing import List
 
 logger = None  # not passing the logger around
 
@@ -17,7 +19,7 @@ def setup_logger():
 
 
 def get_token():
-    home = expanduser("~")
+    home = os.path.expanduser("~")
     try:
         token = open(home + "/.github/my_gh_token.txt").read()
         return token
@@ -56,6 +58,45 @@ def get_chronological_commits(repo):
     return path_to_current_end
 
 
+def commits_to_latex_str(commits: List[Commit.Commit]):
+    latex_string = r"\documentclass{article}"+ "\n\n"
+    latex_string += r"\usepackage[margin=0.5in]{geometry}" + "\n\n"
+
+    # latex_string += r"\usepackage{titlesec}" + "\n\n"
+    # latex_string += r"\titleformat{\section}  % which section command to format" + "\n"
+    # latex_string += r"   {\fontsize{14}{16}\bfseries} % format for whole line" + "\n"
+    # latex_string += r"   {} % how to show number" + "\n"
+    # latex_string += r"   {1em} % space between number and text" + "\n"
+    # latex_string += r"   {} % formatting for just the text" + "\n"
+    # latex_string += r"   [] % formatting for after the text" + "\n\n"
+    
+    latex_string += r"\begin{document}"
+    latex_string += "\n\n"
+
+    for commit in commits:
+        full_message = re.sub(r"_", r"\_", commit.message)
+        full_message = re.sub(r"`", r"'", full_message)
+        header, body = None, None
+        try:
+            header, body = full_message.split("\n\n")
+        except ValueError:
+            header = full_message
+
+        logger.debug("header='%s'", header)
+        logger.debug("body='%s'", body) if body is not None else logger.debug("<No body>") 
+        
+        # commit_message = re.sub(r"\n", r"\\", commit_message)
+        commit_date_time = commit.committer.date.strftime("%Y-%m-%d %H:%M:%S")
+        logger.debug("commit_date_time='%s'", commit_date_time)
+
+        latex_string += r"\textbf{" + commit_date_time+": "+header+"}\n\n" 
+        if body is not None: latex_string += f"{body}\n\n"
+        latex_string += r"\vspace{0.5cm}" + "\n\n"
+        
+    latex_string += r"\end{document}\n"
+    return latex_string
+
+
 def main():
     assert len(sys.argv) == 2
 
@@ -66,7 +107,10 @@ def main():
     repo = gh.get_repo(gh.get_user().login + "/" + sys.argv[1])
 
     commits = get_chronological_commits(repo)
-
+    
+    latex_string = commits_to_latex_str(commits)
+    latex_file = open(f"{repo.name}.tex", "w").write(latex_string)
+    os.system(f"pdflatex.exe {repo.name}.tex")
 
 if __name__ == "__main__":
     main()
